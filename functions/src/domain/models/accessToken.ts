00294a1f3ddbe0e15f769b01fcf73e1c9dac91fa
Giovanni Castillo <giovanni@aventontech.com>
2025-06-10 01:16:51 -0600

    Add detailed comments to domain models for clarity
    - Documented `User`, `Task`, and `AccessToken` interfaces.
    - Explained the purpose and functionality of each property.

--- functions/src/domain/models/accessToken.ts
+++ functions/src/domain/models/accessToken.ts
@@ -1,6 +1,25 @@
+/**
+ * Represents an access token used for
+ * authenticating and authorizing user requests.
+ */
 export interface AccessToken {
+    /**
+     * Unique identifier of the access token.
+     */
     id: string;
+
+    /**
+     * Identifier of the user associated with the access token.
+     */
     userId: string;
+
+    /**
+     * Indicates whether the token has been revoked and is no longer valid.
+     */
     revoked: boolean;
+
+    /**
+     * The expiration date and time of the access token.
+     */
     expiresAt: Date;
 }
--- functions/src/domain/models/user.ts
+++ functions/src/domain/models/user.ts
@@ -1,4 +1,14 @@
+/**
+ * Represents a user in the system.
+ */
 export interface User {
+    /**
+     * Unique identifier of the user.
+     */
     id: string;
+
+    /**
+     * Email address of the user.
+     */
     email: string;
 }
